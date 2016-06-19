65de76434a69fc498640e266b334cd5e152d0b1a
fresk <fresk@yandex-team.ru>
2016-06-20 02:33:42 +0300

    refactor(PlaybackContainer): refactoring tests

--- test/spec/containers/PlaybackContainer.js
+++ test/spec/containers/PlaybackContainer.js
@@ -1,7 +1,7 @@
 import { PlaybackContainer } from 'containers/PlaybackContainer';
 import Playback from 'components/Playback';
 import player from 'lib/player';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Track from 'records/Track';
 
 function setup(props) {
@@ -9,257 +9,236 @@ function setup(props) {
         nextTrack: sinon.spy(),
         reportPlayerError: sinon.spy()
     };
-    const container = mount(
+    const component = shallow(
         <PlaybackContainer {...props} {...handlers} />
     );
 
     return {
         handlers,
-        container
+        component
     };
+}
+
+function mockProps(overrides) {
+    return Object.assign({}, {
+        currentTrack: new Track({
+            id: '100',
+            title: 'title',
+            artist: 'artist',
+            album: 'album',
+            duration: 220
+        })
+    }, overrides);
 }
 
 describe('containers', () => {
     describe('PlaybackContainer', () => {
-        beforeEach(function() {
-            this.sinon.stub(player, 'on');
-        });
-
         it('should render the Playback component', () => {
-            const { container } = setup();
+            const { component } = setup(mockProps());
 
-            expect(container.find(Playback)).to.have.length(1);
+            expect(component.find(Playback)).to.have.length(1);
         });
 
-        describe('_onTimeUpdate', () => {
-            beforeEach(function() {
-                this.mockContext = {
-                    setState: this.sinon.spy(),
-                    state: {
-                        isChangingTime: false
-                    }
-                };
+        describe('timeupdate event ->', () => {
+            it('should update the current time and progress', () => {
+                const { component } = setup(mockProps());
+                const newCurrentTime = 110;
+                const newProgress = 50;
+
+                component.setState({ isChangingTime: false });
+                component.instance()._onTimeUpdate(newCurrentTime, newProgress);
+
+                expect(component.state('currentTime')).to.be.equal(newCurrentTime);
+                expect(component.state('progress')).to.be.equal(newProgress);
             });
 
-            it('should update the current time and progress', function() {
-                PlaybackContainer.prototype._onTimeUpdate.call(this.mockContext, 110, 50);
+            it('should not update the current time and progress when time is changing', () => {
+                const { component } = setup(mockProps());
+                const newCurrentTime = 110;
+                const newProgress = 50;
 
-                expect(this.mockContext.setState).to.have.callCount(1);
-                expect(this.mockContext.setState).to.be.calledWith({
-                    currentTime: 110,
-                    progress: 50
+                component.setState({
+                    isChangingTime: true,
+                    currentTime: 0,
+                    progress: 0
                 });
-            });
+                component.instance()._onTimeUpdate(newCurrentTime, newProgress);
 
-            it('should not update the current time and progress when time is changing', function() {
-                this.mockContext.state.isChangingTime = true;
-
-                PlaybackContainer.prototype._onTimeUpdate.call(this.mockContext, 110, 50);
-
-                expect(this.mockContext.setState).to.have.callCount(0);
+                expect(component.state('currentTime')).to.be.equal(0);
+                expect(component.state('progress')).to.be.equal(0);
             });
         });
 
-        describe('_onTrackEnded', () => {
-            beforeEach(function() {
-                this.mockContext = {
-                    setState: this.sinon.spy(),
-                    props: {
-                        nextTrack: this.sinon.spy()
-                    }
-                };
+        describe('ended event ->', () => {
+            it('should reset the current time', () => {
+                const { component } = setup(mockProps());
+
+                component.setState({ currentTime: 220 });
+                component.instance()._onTrackEnded();
+
+                expect(component.state('currentTime')).to.be.equal(0);
+
             });
 
-            it('should reset the current time', function() {
-                PlaybackContainer.prototype._onTrackEnded.call(this.mockContext);
+            it('should switch on the next track', () => {
+                const { component, handlers } = setup(mockProps());
 
-                expect(this.mockContext.setState).to.have.callCount(1);
-                expect(this.mockContext.setState).to.be.calledWith({
-                    currentTime: 0
-                });
-            });
+                component.instance()._onTrackEnded();
 
-            it('should switch on the next track', function() {
-                PlaybackContainer.prototype._onTrackEnded.call(this.mockContext);
-
-                expect(this.mockContext.props.nextTrack).to.have.callCount(1);
+                expect(handlers.nextTrack).to.have.callCount(1);
             });
         });
 
-        describe('_onPlayerError', () => {
-            beforeEach(function() {
-                this.mockContext = {
-                    props: {
-                        reportPlayerError: this.sinon.spy(),
-                        currentTrack: new Track({
-                            id: 100,
-                            src: 'src'
-                        })
-                    }
-                };
-            });
+        describe('error event ->', () => {
+            it('should report error with right args', () => {
+                const mock = mockProps();
+                const { component, handlers } = setup(mock);
 
-            it('should report error', function() {
-                PlaybackContainer.prototype._onPlayerError.call(this.mockContext);
+                component.instance()._onPlayerError();
 
-                expect(this.mockContext.props.reportPlayerError).to.have.callCount(1);
-                expect(this.mockContext.props.reportPlayerError).to.be.calledWith(
-                    this.mockContext.props.currentTrack.src,
-                    this.mockContext.props.currentTrack.id
+                expect(handlers.reportPlayerError).to.have.callCount(1);
+                expect(handlers.reportPlayerError).to.be.calledWith(
+                    mock.currentTrack.src,
+                    mock.currentTrack.id
                 );
             });
         });
 
-        describe('_handleWindowMouseMove', () => {
+        describe('window mouse move ->', () => {
             beforeEach(function() {
                 this.sinon.stub(window, 'outerWidth', 320);
-                this.mockContext = {
-                    setState: this.sinon.spy(),
-                    props: {
-                        currentTrack: new Track({
-                            duration: 220
-                        })
-                    }
-                };
             });
 
-            it('should reset progress and time when the cursor over the left edge of the window', function() {
-                PlaybackContainer.prototype._handleWindowMouseMove.call(this.mockContext, {
-                    clientX: -100
-                });
+            it('should reset progress and time when the cursor over the left edge of the window', () => {
+                const { component } = setup(mockProps());
 
-                expect(this.mockContext.setState).to.have.callCount(1);
-                expect(this.mockContext.setState).to.be.calledWith({
-                    currentTime: 0,
-                    progress: 0
-                });
+                component.setState({ currentTime: 110, progress: 50 });
+                component.instance()._handleWindowMouseMove({ clientX: -100 });
+
+                expect(component.state('currentTime')).to.be.equal(0);
+                expect(component.state('progress')).to.be.equal(0);
             });
 
-            it('should set the maximum value of time and progress when the cursor over the right edge of the window', function() {
-                PlaybackContainer.prototype._handleWindowMouseMove.call(this.mockContext, {
-                    clientX: 500
-                });
+            it('should set the maximum value of time and progress when the cursor over the right edge of the window', () => {
+                const { component } = setup(mockProps());
 
-                expect(this.mockContext.setState).to.have.callCount(1);
-                expect(this.mockContext.setState).to.be.calledWith({
-                    currentTime: 220,
-                    progress: 100
-                });
+                component.setState({ currentTime: 110, progress: 50 });
+                component.instance()._handleWindowMouseMove({ clientX: 500 });
+
+                expect(component.state('currentTime')).to.be.equal(220);
+                expect(component.state('progress')).to.be.equal(100);
             });
 
-            it('should set the actual value of time and progress when cursor within the window', function() {
-                PlaybackContainer.prototype._handleWindowMouseMove.call(this.mockContext, {
-                    clientX: 160
-                });
+            it('should set the actual value of time and progress when cursor within the window', () => {
+                const { component } = setup(mockProps());
 
-                expect(this.mockContext.setState).to.have.callCount(1);
-                expect(this.mockContext.setState).to.be.calledWith({
-                    currentTime: 110,
-                    progress: 50
-                });
+                component.setState({ currentTime: 0, progress: 0 });
+                component.instance()._handleWindowMouseMove({ clientX: 160 });
+
+                expect(component.state('currentTime')).to.be.equal(110);
+                expect(component.state('progress')).to.be.equal(50);
             });
         });
 
-        describe('_handleProgressClick', () => {
+        describe('click on the progress line ->', () => {
             beforeEach(function() {
                 this.sinon.stub(window, 'outerWidth', 320);
-                this.mockContext = {
-                    setState: this.sinon.spy(),
-                    props: {
-                        currentTrack: new Track({
-                            duration: 220
-                        })
-                    }
-                };
                 this.sinon.stub(player, 'setProgress');
             });
 
-            it('should set the actual value of time and progress when clicked on progress line', function() {
-                PlaybackContainer.prototype._handleProgressClick.call(this.mockContext, {
-                    clientX: 160
-                });
+            it('should set the actual value of time and progress when clicked on progress line', () => {
+                const { component } = setup(mockProps());
 
-                expect(this.mockContext.setState).to.have.callCount(1);
-                expect(this.mockContext.setState).to.be.calledWith({
-                    currentTime: 110,
-                    progress: 50
-                });
+                component.setState({ currentTime: 0, progress: 0 });
+                component.instance()._handleProgressClick({ clientX: 160 });
+
+                expect(component.state('currentTime')).to.be.equal(110);
+                expect(component.state('progress')).to.be.equal(50);
             });
 
-            it('should update the player progress when clicked on progress line', function() {
-                PlaybackContainer.prototype._handleProgressClick.call(this.mockContext, {
-                    clientX: 160
-                });
+            it('should update the player progress when clicked on progress line', () => {
+                const { component } = setup(mockProps());
+
+                component.setState({ currentTime: 0, progress: 0 });
+                component.instance()._handleProgressClick({ clientX: 160 });
 
                 expect(player.setProgress).to.have.callCount(1);
                 expect(player.setProgress).to.be.calledWith(110);
             });
         });
 
-        describe('_handleProgressMouseDown', () => {
+        describe('mouse down on the progress line ->', () => {
             beforeEach(function() {
                 this.sinon.stub(window, 'addEventListener');
-                this.mockContext = {
-                    setState: this.sinon.spy()
-                };
             });
 
-            it('should add event listener `mousemove` on window when mouse down on progress line', function() {
-                PlaybackContainer.prototype._handleProgressMouseDown.call(this.mockContext);
+            it('should add event listener `mousemove` on window when mouse down on progress line', () => {
+                const { component } = setup(mockProps());
+
+                component.instance()._handleProgressMouseDown();
 
                 expect(window.addEventListener).to.be.calledWith('mousemove');
             });
 
-            it('should add event listener `mouseup` on window when mouse down on progress line', function() {
-                PlaybackContainer.prototype._handleProgressMouseDown.call(this.mockContext);
+            it('should add event listener `mouseup` on window when mouse down on progress line', () => {
+                const { component } = setup(mockProps());
+
+                component.instance()._handleProgressMouseDown();
 
                 expect(window.addEventListener).to.be.calledWith('mouseup');
             });
 
-            it('should set the flag `isChangingTime` when mouse down on progress line', function() {
-                PlaybackContainer.prototype._handleProgressMouseDown.call(this.mockContext);
+            it('should set the flag `isChangingTime` when mouse down on progress line', () => {
+                const { component } = setup(mockProps());
 
-                expect(this.mockContext.setState).to.have.callCount(1);
-                expect(this.mockContext.setState).to.be.calledWith({ isChangingTime: true });
+                expect(component.state('isChangingTime')).to.be.equal(false);
+
+                component.instance()._handleProgressMouseDown();
+
+                expect(component.state('isChangingTime')).to.be.equal(true);
             });
         });
 
-        describe('_handleWindowMouseUp', () => {
+        describe('window mouse up ->', () => {
             beforeEach(function() {
                 this.sinon.stub(window, 'removeEventListener');
-                this.mockContext = {
-                    setState: this.sinon.spy(),
-                    state: {
-                        currentTime: 200
-                    }
-                };
                 this.sinon.stub(player, 'setProgress');
             });
 
-            it('should remove event listener `mousemove` when mouse up on window', function() {
-                PlaybackContainer.prototype._handleWindowMouseUp.call(this.mockContext);
+            it('should remove event listener `mousemove` when mouse up on window', () => {
+                const { component } = setup(mockProps());
+
+                component.instance()._handleWindowMouseUp();
 
                 expect(window.removeEventListener).to.be.calledWith('mousemove');
             });
 
-            it('should remove event listener `mouseup` when mouse up on window', function() {
-                PlaybackContainer.prototype._handleWindowMouseUp.call(this.mockContext);
+            it('should remove event listener `mouseup` when mouse up on window', () => {
+                const { component } = setup(mockProps());
+
+                component.instance()._handleWindowMouseUp();
 
                 expect(window.removeEventListener).to.be.calledWith('mouseup');
             });
 
-            it('should reset the flag `isChangingTime` when mouse up on window', function() {
-                PlaybackContainer.prototype._handleWindowMouseUp.call(this.mockContext);
+            it('should reset the flag `isChangingTime` when mouse up on window', () => {
+                const { component } = setup(mockProps());
 
-                expect(this.mockContext.setState).to.have.callCount(1);
-                expect(this.mockContext.setState).to.be.calledWith({ isChangingTime: false });
+                component.setState({ isChangingTime: true });
+                component.instance()._handleWindowMouseUp();
+
+                expect(component.state('isChangingTime')).to.be.equal(false);
             });
 
             it('should update the player progress when mouse up on window', function() {
-                PlaybackContainer.prototype._handleWindowMouseUp.call(this.mockContext);
+                const { component } = setup(mockProps());
+
+                component.instance()._handleWindowMouseUp();
 
                 expect(player.setProgress).to.have.callCount(1);
-                expect(player.setProgress).to.be.calledWith(this.mockContext.state.currentTime);
+                expect(player.setProgress).to.be.calledWith(
+                    component.state('currentTime')
+                );
             });
         });
     });
