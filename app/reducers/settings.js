f456dfefe04d386ca853f243f9ef663e6f99bc37
fresk <fresk@yandex-team.ru>
2016-06-29 14:11:36 +0300

    refactor(settings): remove `switch` from reducer

--- app/reducers/settings.js
+++ app/reducers/settings.js
@@ -2,21 +2,27 @@ import types from 'constants/ActionTypes';
 import Settings from 'records/Settings';
 
 export default function settings(state = new Settings(), action) {
-    switch (action.type) {
-
-        case types.TOGGLE_MINIMIZE:
-            return state.update('minimize', (v) => !v);
-
-        case types.TOGGLE_SHUFFLE:
-            return state.update('shuffle', (v) => !v);
-
-        case types.CHANGE_LOOP_MODE:
-            return state.set('loopMode', action.loopMode);
-
-        case types.CHANGE_VOLUME:
-            return state.set('volume', action.volume);
-
-        default:
-            return state;
+    if (handlers.hasOwnProperty(action.type)) {
+        return handlers[action.type](state, action);
+    } else {
+        return state;
     }
 }
+
+const handlers = {
+    [types.TOGGLE_MINIMIZE](state) {
+        return state.update('minimize', (v) => !v);
+    },
+
+    [types.TOGGLE_SHUFFLE](state) {
+        return state.update('shuffle', (v) => !v);
+    },
+
+    [types.CHANGE_LOOP_MODE](state, action) {
+        return state.set('loopMode', action.loopMode);
+    },
+
+    [types.CHANGE_VOLUME](state, action) {
+        return state.set('volume', action.volume);
+    }
+};
